8bf99fafb6929dd01c5705ecf03ab52270c10ff5
painaunoix <146705501+painaunoix@users.noreply.github.com>
2024-01-29 18:51:46 +0100

    correct infinite jump playeur

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,14 +7,24 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10;
     public float jump = 10;
     public Rigidbody2D rb;
+    public bool onFloor = false;
     void Update()
     {
         float inputX = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && onFloor == true) {
             rb.velocity = new Vector2(rb.velocity.x, jump);
+            onFloor = false;
         }
 
         rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") {
+            onFloor = true;
+        }
+    }
+
 }
