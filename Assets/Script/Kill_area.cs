3cb4bf0934340dc83e84e9a2162c9df90129779b
AymericBretin <146705185+AymericBretin@users.noreply.github.com>
2024-01-31 13:53:12 +0100

    Update Kill Area

--- Assets/Script/Kill_area.cs
+++ Assets/Script/Kill_area.cs
@@ -6,6 +6,8 @@ public class Kill_area : MonoBehaviour
 {
     public GameObject Check_point;
     public HealthBar healthBar;
+
+    public int Time = 0;
     void Start()
     {
         
@@ -17,9 +19,17 @@ public class Kill_area : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player") {
+        if (other.gameObject.tag == "Player" && Time == 0) {
+            Time = 1;
             healthBar.Health -= 1;
             other.transform.position = new Vector3 (Check_point.transform.position.x, Check_point.transform.position.y, Check_point.transform.position.z);
+            StartCoroutine(Timer());
         }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Time = 0;
     }
 }
