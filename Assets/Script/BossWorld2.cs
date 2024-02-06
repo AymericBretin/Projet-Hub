7a1cc1ba0ebe3e91d5b4426803697be6d9b92237
ewan_s <146203302+ewan-schaff@users.noreply.github.com>
2024-02-06 12:59:18 +0100

    adding door and other level

---
+++ Assets/Script/BossWorld2.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossWorld2 : MonoBehaviour
+{
+    public GameObject build; 
+    void Start()
+    {
+        build.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
